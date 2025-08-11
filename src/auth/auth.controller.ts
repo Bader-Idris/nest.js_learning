34d84508cc6f0f1c3a0c96db4d11ada85a7a4518
bader <baidsforbusiness@gmail.com>
2025-08-11 07:16:03 +0300

    feat: learned more about dependency injection, 0:24:## of the video

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,18 +11,36 @@ export class AuthController {
 
 */
 
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('auth') // adding the auth makes it a global prefix route of /auth
 export class AuthController {
   // to instantiate the controller do:
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+    // this.authService.test() // this is how to invoke the function of the service,
+    // ? this is called dependency injection in a nutshell!
+  }
   /* instead of doing
     authService: AuthService above the constructor
     and this.authService = authService inside the constructor
     we do the private syntax above
    */
+
+  @Post('signup') // this is how to make it as a route with http methods
+  signup() {
+    return "I'm signup"; // returned as plain text
+    // so doing a post in postman to: http://localhost:3000/auth/signin
+    // will return our msg!
+    // in the header of X-Powered-By will be Express
+  }
+
+  @Post('signin')
+  login() {
+    return {
+      msg: "I'm login", // returned as json data
+    };
+  }
 }
 
 /* we can do that using the nest/cli
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,4 +9,9 @@ export class AuthService {} // or its name
 import { Injectable } from "@nestjs/common";
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  test() {} // we can call this function in its controller inside the constructor as:
+  // this.authService.test()
+  login () {}
+  signup () {}
+}
