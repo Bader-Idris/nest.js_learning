54caacb4118194d7c81214ca797002fa78ba46c0
bader <baidsforbusiness@gmail.com>
2025-08-12 15:49:37 +0300

    feat: created 403 errors for duplicated properties with built-in ForbiddenException method, signUp is done!

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,12 +6,13 @@ import { Injectable } from '@nestjs/common';
 export class AuthService {} // or its name
 */
 
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 // import { User, Bookmark } from "@prisma/client";
 import { AuthDto } from './dto';
 // we'll use argon instead of bcrypt, because the latter one can only do up to 72 bytes
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
@@ -23,26 +24,35 @@ export class AuthService {
     // generate the password hash
     const hash = await argon.hash(dto.password);
     // save the new user in the db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-      // This select is to isolate returned properties
-      select: {
-        id: true,
-        email: true,
-        createdAt: true,
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        // This select is to isolate returned properties
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      // or we can remove unwanted fields from user object:
+      // delete user.hash;// this didn't work with me!
+
+      // ! If you get a prisma error of type P2021, then you have to do the bunx prisma migrate dev
+
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') { // P2002 is for unique properties
+          throw new ForbiddenException('Credentials taken'); // this forbidden exception is from nestJs
+        }
       }
-    });
-
-    // or we can remove unwanted fields from user object:
-    // delete user.hash;// this didn't work with me!
-
-    // ! If you get a prisma error of type P2021, then you have to do the bunx prisma migrate dev
-
-    // return the saved user
-    return user;
+      throw error;
+    }
   }
   login() {
     return {
